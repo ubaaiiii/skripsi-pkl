7d91732c7d9d6b0e13ac8a49ee563916ed90851d
ubaaiiii <rizqi.ubaidillah15@mhs.ubharajaya.ac.id>
2020-10-14 18:22:51 +0700

    register as siswa

--- app/Views/auth/reg_siswa.php
+++ app/Views/auth/reg_siswa.php
@@ -17,24 +17,24 @@
                             </div>
                             <div class="col-lg-6 col-12 p-0">
                                 <div class="card rounded-0 mb-0 p-2">
-                                    <div class="card-header pt-50 pb-1">
+                                    <div class="card-header pt-50">
                                         <div class="card-title">
-                                            <h4 class="mb-0">Validasi Akun</h4>
+                                            <h4 class="mb-0">Pengaktifan Akun Siswa</h4>
                                         </div>
                                     </div>
-                                    <p class="px-2">Isi form berikut untuk membuat akun.</p>
+                                    <p class="px-2">Isi form berikut.</p>
                                     <div class="card-content">
                                         <div class="card-body pt-0">
                                             <form id="form-auth-siswa">
                                                 <?= csrf_field() ?>
                                                 <input type="hidden" id="cek-akun" name="cek-akun" value="not-validate">
                                                 <div id="cek">
-                                                    <div class="form-label-group">
+                                                    <div class="form-label-group mt-1">
                                                         <input type="number" id="nomor_induk" name="nomor_induk" class="form-control" placeholder="Nomor Induk" required>
                                                         <label for="nomor_induk">Nomor Induk</label>
                                                     </div>
-                                                    <label for="kelas mt-1">Kelas</label>
-                                                    <select class="form-control" id="kelas" name="kelas" required style="width:100%;">
+                                                    <label for="kelas">Kelas</label>
+                                                    <select class="form-control select" id="kelas" name="kelas" required style="width:100%;">
                                                         <option value="">Pilih Salah Satu...</option>
                                                         <?php foreach ($kelas as $k) : ?>
                                                             <option value="<?= $k->msid; ?>"><?= $k->msid; ?></option>
@@ -45,19 +45,19 @@
                                                 </div>
                                                 <div id="cek-done" hidden="hidden">
                                                     <div class="form-label-group">
-                                                        <input type="text" id="username" class="form-control" placeholder="Username">
+                                                        <input type="text" id="username" required class="form-control" placeholder="Username">
                                                         <label for="username">Username</label>
                                                     </div>
                                                     <div class="form-label-group">
-                                                        <input type="email" id="inputEmail" class="form-control" placeholder="Email">
+                                                        <input type="email" id="inputEmail" required class="form-control" placeholder="Email">
                                                         <label for="inputEmail">Email</label>
                                                     </div>
                                                     <div class="form-label-group">
-                                                        <input type="password" id="inputPassword" class="form-control" placeholder="Password">
+                                                        <input type="password" id="inputPassword" required class="form-control" placeholder="Password">
                                                         <label for="inputPassword">Password</label>
                                                     </div>
                                                     <div class="form-label-group">
-                                                        <input type="password" id="inputConfPassword" class="form-control" placeholder="Confirm Password">
+                                                        <input type="password" id="inputConfPassword" required class="form-control" placeholder="Confirm Password">
                                                         <label for="inputConfPassword">Confirm Password</label>
                                                     </div>
                                                 </div>
@@ -71,7 +71,7 @@
                                                                         <i class="vs-icon feather icon-check"></i>
                                                                     </span>
                                                                 </span>
-                                                                <span class=""> Saya setuju mendaftar akun.</span>
+                                                                <span class=""> Saya setuju mengaktifkan akun.</span>
                                                             </div>
                                                         </fieldset>
                                                     </div>
@@ -79,6 +79,39 @@
                                                 <a href="<?= base_url('auth'); ?>" class="btn btn-outline-primary float-left btn-inline mb-50">Login</a>
                                                 <button type="submit" id="btn-cek" class="btn btn-primary float-right btn-inline mb-50">Cek Akun</a>
                                             </form>
+                                            <script>
+                                                $(document).ready(function() {
+                                                    $(".select").select2({
+                                                        allowClear: true,
+                                                        placeholder: "Pilih Salah Satu..",
+                                                    });
+
+                                                    $('#form-auth-siswa').submit(function(e) {
+                                                        e.preventDefault();
+                                                        var data = $(this).serialize(),
+                                                            validasi = $('#cek-akun').val();
+                                                        if (validasi == 'not-validate') {
+                                                            $.ajax({
+                                                                url: "/auth/validate",
+                                                                data: data,
+                                                                type: "post",
+                                                                success: function(resp) {
+                                                                    console.log(resp);
+                                                                }
+                                                            });
+                                                        } else if (validasi == 'validate') {
+                                                            $.ajax({
+                                                                url: "/auth/activate",
+                                                                data: data,
+                                                                type: "post",
+                                                                success: function(resp) {
+                                                                    console.log(resp);
+                                                                }
+                                                            });
+                                                        }
+                                                    })
+                                                })
+                                            </script>
                                         </div>
                                     </div>
                                 </div>
@@ -87,7 +120,6 @@
                     </div>
                 </div>
             </section>
-
         </div>
     </div>
 </div>
