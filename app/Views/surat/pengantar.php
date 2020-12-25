91895e61c67d429b8c89e5b3cb2fed66b79e7c0d
ubaaiiii <rizqi.ubaidillah15@mhs.ubharajaya.ac.id>
2020-12-25 14:24:11 +0700

    format surat pengantar

--- app/Views/surat/pengantar.php
+++ app/Views/surat/pengantar.php
@@ -7,261 +7,127 @@
 	<meta name="description" content="Template surat pengantar pkl SMK Mandalahayu">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+	<style>
+		table.header td {
+			padding: 1px;
+		}
 
+		.isian {
+			border-bottom: 1px dotted;
+			text-align: center;
+		}
+	</style>
 </head>
 
 <body>
 	<br>
-	<style type="text/css">
-		.tg {
-			border-collapse: collapse;
-			border-spacing: 0;
-			margin: 0px auto;
-		}
-
-		.tg td {
-			border-color: black;
-			border-style: solid;
-			border-width: 1px;
-			font-family: Arial, sans-serif;
-			font-size: 14px;
-			overflow: hidden;
-			padding: 0px 1px;
-			word-break: normal;
-		}
-
-		.tg th {
-			border-color: black;
-			border-style: solid;
-			border-width: 1px;
-			font-family: Arial, sans-serif;
-			font-size: 14px;
-			font-weight: normal;
-			overflow: hidden;
-			padding: 0px 1px;
-			word-break: normal;
-		}
-
-		.tg .tg-0r18 {
-			border-color: inherit;
-			font-size: 14px;
-			text-align: center;
-			vertical-align: top
-		}
-
-		.tg .tg-zd5i {
-			border-color: inherit;
-			font-size: 14px;
-			text-align: left;
-			vertical-align: top
-		}
-
-		.tg .tg-dbtv {
-			border-color: inherit;
-			font-size: 16px;
-			font-weight: bold;
-			text-align: left;
-			vertical-align: top
-		}
-
-		.tg .tg-umgj {
-			border-color: inherit;
-			font-size: 14px;
-			font-weight: bold;
-			text-align: center;
-			vertical-align: top
-		}
-
-		.tg .tg-7z5x {
-			border-color: inherit;
-			font-size: 14px;
-			font-style: italic;
-			text-align: left;
-			vertical-align: top
-		}
-	</style>
-	<table class="tg" style="undefined;table-layout: fixed; width: 851px">
-		<colgroup>
-			<col style="width: 92px">
-			<col style="width: 11px">
-			<col style="width: 58px">
-			<col style="width: 77px">
-			<col style="width: 45px">
-			<col style="width: 48px">
-			<col style="width: 106px">
-			<col style="width: 148px">
-			<col style="width: 59px">
-			<col style="width: 31px">
-			<col style="width: 154px">
-			<col style="width: 22px">
-		</colgroup>
-		<tbody>
-			<tr>
-				<td class="tg-zd5i">Nomor</td>
-				<td class="tg-zd5i">:</td>
-				<td class="tg-zd5i" colspan="10">191/SMK-YMH/PKL/E.5/X/2019</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i">Lamp</td>
-				<td class="tg-zd5i">:</td>
-				<td class="tg-zd5i">-</td>
-				<td class="tg-zd5i" colspan="9"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i">Hal</td>
-				<td class="tg-zd5i">:</td>
-				<td class="tg-zd5i" colspan="10">Pengantar Praktik Kerja/Magang</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i">Kepada</td>
-				<td class="tg-zd5i" colspan="11"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i">Yth</td>
-				<td class="tg-zd5i">:</td>
-				<td class="tg-dbtv" colspan="10">PT. Duta Hita Jaya</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="10">di Bekasi</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="11">Dengan Hormat,</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="6">Sesuai dengan informasi yang kami terima tanggal</td>
-				<td class="tg-0r18" colspan="2">22 Oktober 2020</td>
-				<td class="tg-zd5i">No.</td>
-				<td class="tg-0r18">999999999999999</td>
-				<td class="tg-zd5i"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="2">Perihal</td>
-				<td class="tg-0r18" colspan="8">Praktik Kerja Industri (PRAKERIN)</td>
-				<td class="tg-zd5i"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="4">Dengan ini kami kirimkan </td>
-				<td class="tg-0r18" colspan="2">3 (Tiga)</td>
-				<td class="tg-zd5i">orang siswa.</td>
-				<td class="tg-zd5i" colspan="4"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-umgj" colspan="2">NO</td>
-				<td class="tg-umgj" colspan="3">NAMA</td>
-				<td class="tg-umgj">NIS</td>
-				<td class="tg-umgj" colspan="2">KELAS / JURUSAN</td>
-				<td class="tg-umgj" colspan="3">KETERANGAN</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-0r18" colspan="2">1</td>
-				<td class="tg-zd5i" colspan="3">ASDF</td>
-				<td class="tg-0r18">123123</td>
-				<td class="tg-0r18" colspan="2">ASDFASDF</td>
-				<td class="tg-zd5i" colspan="3">ASDFASDFASDFASDF</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="11">Untuk melaksanakan Praktik Kerja Industri pada perusahaan yang Bapak / Ibu pimpin</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="2">selama</td>
-				<td class="tg-0r18" colspan="3">3 (Tiga) Bulan</td>
-				<td class="tg-zd5i">mulai tanggal</td>
-				<td class="tg-0r18" colspan="5">22 oktober 2020</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="3">sampai dengan</td>
-				<td class="tg-0r18" colspan="4">22 oktober 2020</td>
-				<td class="tg-zd5i" colspan="4"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="11">Pada akhir Praktik Kerja Industri, siswa diwajibkan untuk membuat laporan yang disetujui oleh pimpinan perusahaan.</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-zd5i" colspan="11">Demikian pengantar ini kami sampaikan atas perhatian dan kerjasama yang baik, kami ucapkan terima kasih.</td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="12"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-				<td class="tg-0r18" colspan="3">Bekasi, 22 oktober 2020</td>
-				<td class="tg-zd5i"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-				<td class="tg-0r18" colspan="3">Kepala Sekolah</td>
-				<td class="tg-zd5i"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-				<td class="tg-0r18" colspan="3" rowspan="4"><img src="https://i.ibb.co/Tm1wvh0/image.png" width="151" height="113"></td>
-				<td class="tg-zd5i" rowspan="4"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i" colspan="8"></td>
-				<td class="tg-umgj" colspan="3">Dra. Hj. Sulistyawati</td>
-				<td class="tg-zd5i"></td>
-			</tr>
-			<tr>
-				<td class="tg-7z5x" colspan="3">Contact Person:</td>
-				<td class="tg-zd5i" colspan="9"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-7z5x" colspan="7">E. Jaenal, S.Pd /Ketua Hubin (085697938659/081218508648</td>
-				<td class="tg-zd5i" colspan="4"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-7z5x" colspan="7">Qibtiyah Utami, S.Pd - Staff Hubin (081213690707)</td>
-				<td class="tg-zd5i" colspan="4"></td>
-			</tr>
-			<tr>
-				<td class="tg-zd5i"></td>
-				<td class="tg-7z5x" colspan="7">Muhammad Ramadhani - Staff TU Hubin (085692664081)</td>
-				<td class="tg-zd5i" colspan="4"></td>
-			</tr>
-		</tbody>
+	<a href="/jadwal/print" target="_blank">Print</a>
+	<table align="left" width="100%" style="font-size: 12px" class="header">
+		<tr>
+			<td width="10%">Nomor</td>
+			<td width="1%">:</td>
+			<td>191/SMK-YMH/PKL/E.5/X/2019</td>
+		</tr>
+		<tr>
+			<td>Lamp</td>
+			<td>:</td>
+			<td>-</td>
+		</tr>
+		<tr>
+			<td>Hal</td>
+			<td>:</td>
+			<td>Pengantar Praktik Kerja/Magang</td>
+		</tr>
+		<tr>
+			<td colspan=3>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>Kepada</td>
+			<td colspan=2>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>Yth</td>
+			<td>:</td>
+			<td>
+				<b>
+					<h3>PT. DUTA JAYA ABADI</h3>
+				</b>
+			</td>
+		</tr>
+		<tr>
+			<td colspan=2>&nbsp;</td>
+			<td>di Bekasi</td>
+		</tr>
+	</table>
+	<br>
+	<table align="left" width="100%" style="margin-left:45px;font-size:12px;">
+		<tr>
+			<td colspan=11>Dengan hormat,</td>
+		</tr>
+		<tr>
+			<td colspan=11>&nbsp;</td>
+		</tr>
+		<tr>
+			<td colspan=6 width="50%">Sesuai dengan informasi yang kami terima tanggal</td>
+			<td colspan=3 width="20%" class="isian">22 Oktober 2020</td>
+			<td width="10%">No.</td>
+			<td colspan=2 width="20%" class="isian">&nbsp;</td>
+		</tr>
+		<tr>
+			<td colspan=2>Perihal: </td>
+			<td colspan=9 class="isian">Praktik Kerja Industri (PRAKERIN)</td>
+		</tr>
+		<tr>
+			<td colspan=3>Dengan ini kami kirimkan</td>
+			<td colspan=2 class="isian">3 (Tiga)</td>
+			<td colspan=6>orang siswa</td>
+		</tr>
+	</table>
+	<br>
+	<table align="left" width="100%" style="margin-left:45px;font-size:12px;">
+		<tr>
+			<th>
+				NO
+			</th>
+			<th>
+				NAMA
+			</th>
+			<th>
+				NOMOR INDUK
+			</th>
+			<th>
+				KELAS / JURUSAN
+			</th>
+			<th>
+				KETERANGAN
+			</th>
+		</tr>
+		<tr height="1px">
+			<td colspan=5>
+				&nbsp;
+			</td>
+		</tr>
+		<tr>
+			<td align="center">1</td>
+			<td>KASKDFKJHKJH</td>
+			<td align="center">123123123</td>
+			<td align="center">LKASJDKFJ</td>
+			<td>KLAJSDHLGHLASKDHFKGAKLJSHDG</td>
+		</tr>
+		<tr>
+			<td align="center">2</td>
+			<td>KASKDFKJHKJH</td>
+			<td align="center">123123123</td>
+			<td align="center">LKASJDKFJ</td>
+			<td>KLAJSDHLGHLASKDHFKGAKLJSHDG</td>
+		</tr>
+		<tr>
+			<td align="center">3</td>
+			<td>KASKDFKJHKJH</td>
+			<td align="center">123123123</td>
+			<td align="center">LKASJDKFJ</td>
+			<td>KLAJSDHLGHLASKDHFKGAKLJSHDG</td>
+		</tr>
 	</table>
 
 </body>
